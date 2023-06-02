c1359c569fec3c6a3cabb4bd0cf8f675f807a088
hby <hby@DESKTOP-O7J4V5L>
2023-06-02 17:34:38 +0800

    添加项目文件。

---
+++ Browser.xaml.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+
+namespace mgtv_fulllscreen
+{
+    /// <summary>
+    /// Browser.xaml 的交互逻辑
+    /// </summary>
+    public partial class Browser : Window
+    {
+        private string url;
+        private string password = "mgtv";
+        private string input = "";
+        public Browser(string url, string password)
+        {
+            InitializeComponent();
+
+            Keyboard.AddKeyDownHandler(this, OnKeyDown);
+
+            this.url = url;
+            this.password = password.ToLower();
+            this.Loaded += MainWindow_Loaded;
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            browser.Address = this.url;
+        }
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            
+            // 判断按下的是否连续按下了密码
+            if (char.IsDigit((char)KeyInterop.VirtualKeyFromKey(e.Key)) || char.IsLetter((char)KeyInterop.VirtualKeyFromKey(e.Key)))
+            {
+                this.input = (this.input + e.Key).ToLower();
+                if(!password.StartsWith(this.input))
+                {
+                    this.input = "";
+                }
+                else if (this.input == this.password)
+                {
+                    this.Close();
+                }
+            }
+            if (Keyboard.Modifiers != ModifierKeys.None)
+            {
+                e.Handled = true;
+                return;
+            }
+            
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace mgtv_fulllscreen
+{
+    /// <summary>`
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        { 
+            InitializeComponent();
+           urlTextBox.Text = Properties.Settings.Default.url;
+        }
+        private void open(Object sender, RoutedEventArgs e)
+        {
+            Browser b = new Browser(urlTextBox.Text, passwordTextBox.Text);
+            b.Closed += B_Closed;
+            this.Hide();
+            b.Show();
+        }
+
+        private void B_Closed(object sender, EventArgs e)
+        {
+           this.Show();
+        }
+
+        private void urlTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                Properties.Settings.Default.url = urlTextBox.Text;
+                Properties.Settings.Default.Save();
+                this.open(sender, e);
+            }
+        }
+    }
+}
